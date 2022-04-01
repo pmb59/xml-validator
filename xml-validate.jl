f434c1cc839b86257317d6c9dcd03dc8ec40a38a
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-04-01 23:09:57 +0100

    Create xml-validate.jl

---
+++ xml-validate.jl
@@ -0,0 +1,29 @@
+using LightXML
+ 
+const Xptr = LightXML.Xptr
+ 
+function validate(url::String, schemafile::String)
+    ctxt = ccall((:xmlSchemaNewParserCtxt, LightXML.libxml2), Xptr, (Cstring,), schemafile)
+    ctxt != C_NULL || throw(LightXML.XMLNoRootError())
+    schema = ccall((:xmlSchemaParse, LightXML.libxml2), Xptr, (Xptr,), ctxt)
+    schema != C_NULL || throw(LightXML.XMLNoRootError())
+    ccall((:xmlSchemaFreeParserCtxt, LightXML.libxml2), Cvoid, (Xptr,), ctxt)
+    ctxt = ccall((:xmlSchemaNewValidCtxt, LightXML.libxml2), Xptr, (Xptr,), schema)
+    err = ccall((:xmlSchemaValidateFile, LightXML.libxml2), 
+        Cint, (Ptr{LightXML.xmlBuffer}, Cstring), ctxt, url)
+    return err == 0 ? true : false
+end
+ 
+function xsdvalidatexml()
+    if length(ARGS) != 2
+        println("		Usage: julia validate_xml.jl xmlfilename xsdfilename")
+    elseif validate(ARGS[1], ARGS[2])
+        println("File ", ARGS[1], " validates as correct XML using the XSD file ", ARGS[2], ".")
+    else
+        println("File ", ARGS[1]. "does not validate.")
+        # exit with error
+        exit(1)
+    end
+end
+ 
+xsdvalidatexml()
