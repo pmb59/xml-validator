eb7f66d4099187209493f5163d9ede3ba43b8000
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-04-06 00:16:51 +0100

    Update tests.jl

--- tests.jl
+++ tests.jl
@@ -1,18 +1,11 @@
-ENV["RUNSCRIPT"] = true
+#!/usr/bin/env julia
 
-
-if haskey(ENV, "RUNSCRIPT")
+test_case = [ "shiporder", "configuration" ] 
   
-  test_case = [ "shiporder", "configuration" ] 
-  
-  foreach( test_case ) do f
-    if isfile("./test_data/" * f * ".xml") == false || isfile("./test_data/" * f * ".xsd") == false
-      exit(1)
-    else
-      print( "files for test " * f * " ready\n" )
-    end
+foreach( test_case ) do f
+  if isfile("./test_data/" * f * ".xml") == false || isfile("./test_data/" * f * ".xsd") == false
+    exit(1)
+  else
+    print( "files for test " * f * " ready\n" )
   end
-  
 end
-
-
